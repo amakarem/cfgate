c6b0d3a8ccd2d2092d751fdec06616dd69cb6dae
Ahmed Elaaser <44022058+amakarem@users.noreply.github.com>
2021-11-04 12:59:42 +0200

    Update Index.php

--- app/Models/Index.php
+++ app/Models/Index.php
@@ -2,9 +2,51 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Traits\Resizable;
+use TCG\Voyager\Traits\Translatable;
 
 class Index extends Model
 {
-    protected $table = 'index';
+    use Translatable;
+    use Resizable;
+
+    protected $translatable = ['name', 'excerpt', 'body', 'slug'];
+
+    const ACTIVE = 'ACTIVE';
+
+    protected $guarded = [];
+
+    public function save(array $options = [])
+    {
+        // If no author has been assigned, assign the current user's id as the author of the post
+        if (!$this->author_id && Auth::user()) {
+            $this->author_id = Auth::user()->getKey();
+        }
+
+        parent::save();
+    }
+
+    /**
+     * Scope a query to only published scopes.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     *
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopePublished(Builder $query)
+    {
+        return $query->where('status', '=', static::ACTIVE);
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function category()
+    {
+        return $this->belongsTo(Voyager::modelClass('Category'));
+    }
 }
