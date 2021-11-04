6553c590f6d3f8f219d1c6541620bcc10a584cb8
Ahmed Elaaser <44022058+amakarem@users.noreply.github.com>
2021-11-04 17:31:19 +0200

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,6 +28,7 @@ class HomeController extends Controller
         $userID = Auth::user()->id;
         $headers = [];
         $data = DB::table('trades')->where('owner', $userID)->orderBy('code')->get();
+        $data = $data->toArray();
         foreach ($data as $row) {
                     print_r($row);
         die();
