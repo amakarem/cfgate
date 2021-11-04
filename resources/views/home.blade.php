8222ce3aa1c5d8a80e6f341333e23379d2e16c31
Ahmed Elaaser <44022058+amakarem@users.noreply.github.com>
2021-11-04 17:18:02 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ $title }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                  <table class="table table-bordered">
+                              <thead class="bg-light">
+                                <tr>
+                                          @if (isset($headers))
+                                            <th scope='col' class='font-weight-bold'>Line#</th>
+                                            @foreach($headers as $header)
+                                                <th scope='col' class='font-weight-bold'>{{ucfirst($header)}}</th>
+                                            @endforeach
+                                          @endif
+                                </tr>
+                              </thed>
+                              <tbody>
+                                          @if (isset($data) && isset($headers))
+                                            @foreach($data as $row)
+                                            <tr>
+                                              <td scope='col' class='font-weight-bold'>{{ $loop->iteration }}</td>
+                                              @foreach($headers as $header)
+                                                @if (isset($row[$header]))
+                                                  <td scope='col' class='font-weight-bold'>{{$row[$header]}}</td>
+                                                @else
+                                                  <td></td>
+                                                @endif
+                                              @endforeach
+                                            </tr>
+                                            @endforeach
+                                          @endif
+                              </tbody>
+                  </table>
                 </div>
             </div>
         </div>
